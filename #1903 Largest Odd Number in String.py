590c672a9030c0c7d26008107aeb02fdc8d25dfa
K. M <90254516+km1610@users.noreply.github.com>
2023-12-07 19:19:57 +0530

    Update #1903 Largest Odd Number in String.py

--- #1903 Largest Odd Number in String.py
+++ #1903 Largest Odd Number in String.py
@@ -1,11 +1,7 @@
 class Solution(object):
     def largestOddNumber(self, num):
-        r = len(num)-1
-        while r>=0:
-            if int(num[r])%2==0:
-                r-=1
-            else:
-                return num[0:r+1]
-        return '' 
-            
+        for r in range(1,len(num)+1):
+            if int(num[-r])%2==1:
+                return num[:len(num)-r+1]
+        return ''
         
