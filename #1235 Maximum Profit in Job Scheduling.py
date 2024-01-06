d209a900454ee3facae9e629d2d93924adeda85f
K. M <90254516+km1610@users.noreply.github.com>
2024-01-06 20:13:20 +0530

    Create #1235 Maximum Profit in Job Scheduling.py

---
+++ #1235 Maximum Profit in Job Scheduling.py
@@ -0,0 +1,8 @@
+        
+        dp = [[0,0]]                                         
+        f = lambda x: dp[bisect_left(dp,[x+1])-1][1]          
+        for e, s, p in sorted(zip(endTime,startTime, profit)): 
+                                                               
+            dp.append([e, max(f(e),p+f(s))])                  
+                    
+        return dp[-1][1]  
