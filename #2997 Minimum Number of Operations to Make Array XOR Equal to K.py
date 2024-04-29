fd7a7ba5b478e37a21242b22e010760855feb9ce
K. M <90254516+km1610@users.noreply.github.com>
2024-04-29 14:37:36 +0530

    Update #2997 Minimum Number of Operations to Make Array XOR Equal to K.py

--- #2997 Minimum Number of Operations to Make Array XOR Equal to K.py
+++ #2997 Minimum Number of Operations to Make Array XOR Equal to K.py
@@ -7,17 +7,13 @@ def dectobin(n):
 
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
-
         a = nums[0]
         for i in range(1,len(nums)):
             a = a^nums[i]
-        b1 = dectobin(a)
-        b2 = dectobin(k)
-        m = max(len(b1),len(b2))
-        b1 = '0'*(m-len(b1)) + b1
-        b2 = '0'*(m-len(b2)) + b2
+        b = a^k
+        b = dectobin(b)
         d = 0
-        for i in range(m):
-            if b1[i]!=b2[i]:
+        for i in range(len(b)):
+            if b[i]=='1':
                 d+=1
         return d
