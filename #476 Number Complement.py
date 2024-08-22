b29510b485caa3cd1afbbb06bc6f8cba631515af
K. M. <90254516+km1610@users.noreply.github.com>
2024-08-22 13:25:44 +0530

    Update #476 Number Complement.py

--- #476 Number Complement.py
+++ #476 Number Complement.py
@@ -1,27 +1,4 @@
+import math
 class Solution:
     def findComplement(self, num: int) -> int:
-
-        def dectobin(num):
-            return bin(num)[2::]
-        
-        def bintodec(num):
-            s = 0
-            power = 0
-            for i in range(1,len(num)+1):
-                s+= int(num[-i])*(2**power)
-                power+=1
-            return s
-        
-        def complement(num):
-            s = ''
-            for i in num:
-                if i=='1':
-                    s+='0'
-                else:
-                    s+='1'
-            return s
-
-        num = dectobin(num)
-        num = complement(num)
-        num = bintodec(num)
-        return num
+        return num^((2**(int(math.log(num,2)+1)))-1)
