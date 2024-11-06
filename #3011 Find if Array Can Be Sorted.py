f6baaf725169b03f818176ae6da3740cf900e617
K. M. <90254516+km1610@users.noreply.github.com>
2024-11-07 04:12:26 +0530

    Create #3011 Find if Array Can Be Sorted.py

---
+++ #3011 Find if Array Can Be Sorted.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canSortArray(self, nums: List[int]) -> bool:
+        def set_bits(num):
+            bits = 0
+            t = num
+            while num:
+                if num%2:
+                    bits += 1
+                num = num >> 1
+            return bits
+
+        n = len(nums)
+        for i in range(n):
+            for j in range(n-1):
+                if nums[j]>nums[j+1]:
+                    if set_bits(nums[j]) == set_bits(nums[j+1]):
+                        nums[j],nums[j+1] = nums[j+1], nums[j]
+                    else:
+                        return False
+        return True
