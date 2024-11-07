90dc3168c773bb9c50a307d9ed6ca0c69e3545e6
K. M. <90254516+km1610@users.noreply.github.com>
2024-11-07 16:51:32 +0530

    Update #2275 Largest Combination With Bitwise AND Greater Than Zero.py

--- #2275 Largest Combination With Bitwise AND Greater Than Zero.py
+++ #2275 Largest Combination With Bitwise AND Greater Than Zero.py
@@ -1,19 +1,14 @@
 class Solution:
     def largestCombination(self, candidates: List[int]) -> int:
-        max_num = max(candidates)
-        max_bits = math.ceil(math.log(max_num, 2)) + 1
         bit_set = {}
-        for i in range(max_bits+1):
+        for i in range(32):
             bit_set[i] = 0
         
         for i in candidates:
             binary = bin(i)[2::]
-            # print(i, binary)
             for j in range(len(binary)):
                 if binary[j]=='1':
-                    # print(i, len(binary) - j - 1)
                     bit_set[len(binary) - j - 1] += 1
-        # print(bit_set)
         max_bit_and = 0
         for i in bit_set:
             max_bit_and = max(max_bit_and, bit_set[i])
